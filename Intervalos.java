b1edb86fa5331b0381621221afb8330eb52d57b4
guilhermepdc <guilherme.carvalho@edu.pucrs.br>
2020-05-27 21:01:41 -0300

    mais uma tentativa e mais um fracasso

---
+++ Intervalos.java
@@ -0,0 +1,9 @@
+public class Intervalos {
+  private Integer minimo;
+  private Integer maximo;
+
+  public Intervalos(Integer min, Integer max) {
+    this.minimo = min;
+    this.maximo = max;
+  }
+}
--- SDM.java
+++ SDM.java
@@ -1,29 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SDM {
   public static void main(String[] args) {
 
-    int[] s = {4, 6, 13, 4, 2, 6, 7, 9, 1, 3, 9};
-    int[] f = {8, 7, 14, 5, 4, 9, 10, 11, 6, 13, 12};
+    ArrayList<Integer> minMax = new ArrayList<Integer>();
+    minMax={{4,6},{1,13},{2,8},{7,9}};
+    // int[] f = {8, 7, 14, 5, 4, 9, 10, 11, 6, 13, 12};
 
-    result = sdmGuloso(s, f, n);
+    List<Intervalos> listaIntervalos = minMax;
 
-    millis = System.currentTimeMillis();
-    System.out.println("solução SDM-Guloso: " + Arrays.toString(result));
-    millisEnd = System.currentTimeMillis();
-    total = millisEnd - millis;
-    System.out.println("TEMPO: " + total);
+    System.out.println(listaIntervalos.get(1).minimo);
+
+    // result = sdmGuloso(s, f, n);
+
+    // millis = System.currentTimeMillis();
+    // System.out.println("solução SDM-Guloso: " + Arrays.toString(result));
+    // millisEnd = System.currentTimeMillis();
+    // total = millisEnd - millis;
+    // System.out.println("TEMPO: " + total);
     // System.out.println("COUNT: " + count + "\n");
 
   }
 
   // solução recursiva
-  public static int[] sdmGuloso(int[] s, int[] f, n) {
-    1   f[0] = −∞
-    2   X ← { }
-    3   i ← 0
-    4   para k ← 1 até n faça
-    5         if (s[k] > f[i])
-    6              então X ← X ∪ {k}
-    7                       i ← k
-    8   return X;
+
+public static Set sdm(List<Intervalos> listaIntervalos) {
+  if(listaIntervalos == null || listaIntervalos.size() == 0)
+      return null;
+  if(listaIntervalos.size() == 1)
+  return new HashSet<Intervalos>(listaIntervalos);
+
+  Collections.sort(listaIntervalos);
+  Set result = new HashSet<Intervalos>();
+  int index = 0;
+  int lastMax = Integer.MIN_VALUE;
+  while(index < listaIntervalos.size()){
+      if(listaIntervalos.get(index).minimo >= lastMax){
+          result.add(listaIntervalos.get(index));
+          lastMax = listaIntervalos.get(index).maximo;
+      }
+  index++;
   }
+  return result;
 }
