be60ee8717a4c6b1ef8f5577cc7d4b5bab9dba0a
Matheus <matheus.tosin@edu.pucrs.br>
2020-06-03 20:55:54 -0300

    almost

--- SDM.js
+++ SDM.js
@@ -1,15 +1,28 @@
-    let listaS = [4, 6, 13, 4, 2, 6, 7, 9, 1, 3, 9];
-    let listaF = [8, 7, 14, 5, 4, 9, 10, 11, 6, 13, 12];
-
+    let listaS = [2, 4, 1, 6, 4, 6, 7,   9, 9,   3, 13  ] ;
+    let listaF = [4, 5, 6, 7, 8, 9, 10, 11,  12,  13, 14];
     console.log(Guloso(listaS, listaF, 11));
+    
+    // function Guloso(s, f, n) {
+    //     let x = []
+    //     let i= 0
+    //     while(i <= n) {
+    //         x.push(f[i])
+    //         var k = i + 1
+    //         while( k <= n && s[k] < f[i]) {
+    //             k = k+1
+    //         }
+    //         i = k
+    //     }
+    //     return x;
+    // }
 
     function Guloso (s, f, n) {
-        f[0] = 0;
+        f[0] = Number.MIN_VALUE;
         let x = [];
         let i = 0;
         for(let k = 1; k < n; k++) {
             if(s[k] > f[i]) {
-                x.push(f[k])
+                x.push({S: s[k] , F: f[k]})
                 i = k
             }
         }
